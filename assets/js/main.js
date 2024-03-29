6151de9db29ab910cc52f6a5cf971c2fabb437f7
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-03-29 15:39:18 +0000

    Add event listener to submit the form based on condition

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,18 +4,25 @@ let errorMessages = {
     "invalidEmail": "Valid email required",
 };
 
+let emptyFieldError = true;
+let invalidEmailError = true;
+
 // DOM elements
 const emailInput = document.querySelector('#email');
 const errorPlaceholder = document.querySelector('#error-placeholder');
+const newsletterForm = document.querySelector('#newsletter-form');
+const submitFormBtn = document.querySelector('#submit-button');
 
 // This event listener validates the email input field in real-time, displaying an error message if it's empty.
 emailInput.addEventListener('input', () => {
     if (emailInput.value.length < 1) {
         errorPlaceholder.textContent = errorMessages.emptyField;
         emailInput.classList.add('has-error');
+        emptyFieldError = true;
     } else {
         errorPlaceholder.textContent = '';
         emailInput.classList.remove('has-error');
+        emptyFieldError = false;
     }
 });
 
@@ -25,5 +32,16 @@ emailInput.addEventListener('focusout', () => {
     if (emailInput.value.length > 1 && !emailInput.value.match(emailReg)) {
         errorPlaceholder.textContent = errorMessages.invalidEmail;
         emailInput.classList.add('has-error');
+        invalidEmailError = true;
+    } else {
+        invalidEmailError = false;
+    }
+});
+
+// This event listener submitted the form when there is no error
+submitFormBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    if(!emptyFieldError && !invalidEmailError) {
+        newsletterForm.submit();
     }
 });
