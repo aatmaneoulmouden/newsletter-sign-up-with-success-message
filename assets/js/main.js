5d14142deeba50b647896c399790a38b3543bc75
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-03-30 15:28:10 +0000

    Toggle form and message popup functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-// Error messages
+// Errors
 let errorMessages = {
     "emptyField": "The field is left empty",
     "invalidEmail": "Valid email required",
@@ -8,10 +8,20 @@ let emptyFieldError = true;
 let invalidEmailError = true;
 
 // DOM elements
+const form = document.querySelector('#form');
+const message = document.querySelector('#message');
 const emailInput = document.querySelector('#email');
+const emailOutput = document.querySelector('#email-output');
 const errorPlaceholder = document.querySelector('#error-placeholder');
 const newsletterForm = document.querySelector('#newsletter-form');
 const submitFormBtn = document.querySelector('#submit-button');
+const dismissMessageBtn = document.querySelector('#dismiss-msg-btn');
+
+// Functions
+const toggleShowedClass = (removeFrom, addTo) => {
+    removeFrom.classList.remove('showed');
+    addTo.classList.add('showed');
+};
 
 // This event listener validates the email input field in real-time, displaying an error message if it's empty.
 emailInput.addEventListener('input', () => {
@@ -41,7 +51,18 @@ emailInput.addEventListener('focusout', () => {
 // This event listener submitted the form when there is no error
 submitFormBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    if(!emptyFieldError && !invalidEmailError) {
-        newsletterForm.submit();
+    if(emptyFieldError) {
+        errorPlaceholder.textContent = errorMessages.emptyField;
+        emailInput.classList.add('has-error');
+        emptyFieldError = true;
+    }
+
+    if (!emptyFieldError && !invalidEmailError) {
+        console.log(emailInput.value);
+        emailOutput.textContent = emailInput.value;
+        toggleShowedClass(form, message);
     }
 });
+
+// This event listener dissmissed the message popup when click on 'Dissmiss message' button
+dismissMessageBtn.addEventListener('click', () => toggleShowedClass(message, form));
